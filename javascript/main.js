34122247efbf799dbdf117012061e051bc7e1b2f
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-07-21 13:59:55 -0400

    Added block strength and decay over time

--- javascript/main.js
+++ javascript/main.js
@@ -15,6 +15,7 @@ var data = {
 for (var i = 0; i < 100; i ++) {
       data.world.blocks.push(
             {
+                  "strength": 100 + Math.round(Math.random() * 25),
                   "location": {
                         "x": Math.round(Math.random() * 10),
                         "y": Math.round(Math.random() * 10)
@@ -23,23 +24,31 @@ for (var i = 0; i < 100; i ++) {
       );
 }
 
-function render() {
+function update() {
       canvas.width = window.innerWidth;
       canvas.height = window.innerHeight;
 
       context.fillStyle = "rgba(255, 255, 255, 1)";
       context.fillRect(0, 0, canvas.width, canvas.height);
 
+      // Render blocks
       for (var i = 0; i < data.world.blocks.length; i ++) {
-            // Render blocks
-            context.fillStyle = "rgba(0, 0, 0, 1)";
+            block = data.world.blocks[i];
+
+            context.fillStyle = "rgba(0, 0, 0, " + (block.strength / 100) + ")";
             context.fillRect(
-                  data.world.blocks[i].location.x * data.settings.zoom,
-                  data.world.blocks[i].location.y * data.settings.zoom,
+                  block.location.x * data.settings.zoom,
+                  block.location.y * data.settings.zoom,
                   data.settings.zoom,
                   data.settings.zoom
             );
+
+            block.strength -= Math.random() * 0.01;
+
+            if (block.strength <= 0) {
+                  data.world.blocks.splice(i, 1);
+            }
       }
 }
 
-window.setInterval(render, 10);
+window.setInterval(update, 10);
