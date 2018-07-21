49889d3314ef763321a481ac0d0632f0246f66a4
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-07-21 13:44:36 -0400

    Added rendering function and render loop

--- javascript/main.js
+++ javascript/main.js
@@ -3,9 +3,6 @@
 const canvas = document.querySelector("#canvas");
 const context = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
 var data = {
       "settings": {
             "zoom": 25
@@ -26,13 +23,23 @@ for (var i = 0; i < 100; i ++) {
       );
 }
 
-for (var i = 0; i < data.world.blocks.length; i ++) {
-      // Render blocks
-      context.fillStyle = "rgba(0, 0, 0, 1)";
-      context.fillRect(
-            data.world.blocks[i].location.x * data.settings.zoom,
-            data.world.blocks[i].location.y * data.settings.zoom,
-            data.settings.zoom,
-            data.settings.zoom
-      );
+function render() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      context.fillStyle = "rgba(255, 255, 255, 1)";
+      context.fillRect(0, 0, canvas.width, canvas.height);
+
+      for (var i = 0; i < data.world.blocks.length; i ++) {
+            // Render blocks
+            context.fillStyle = "rgba(0, 0, 0, 1)";
+            context.fillRect(
+                  data.world.blocks[i].location.x * data.settings.zoom,
+                  data.world.blocks[i].location.y * data.settings.zoom,
+                  data.settings.zoom,
+                  data.settings.zoom
+            );
+      }
 }
+
+window.setInterval(render, 10);
